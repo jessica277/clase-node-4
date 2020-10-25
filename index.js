631b489165d7505a391bdd2c7990c16f6a47ddc2
jessica277 <jessicalizbethsotoquevedo@gmail.com>
2020-10-25 13:22:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,30 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const path = require('path');
-
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password : 'root',
-  database : 'clasenode'
-});
-
-//Conectarnos a la base de datos
-connection.connect();
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-app.get('/', (req, res) => {
-  res.send("Bienvenido a la API de Rogelio Noris");
-});
-
-app.get('/personajes', (req, res) => {
-  //Consultar los personajes
-  connection.query('SELECT * FROM personajes', function (error, results, fields) {
-    if(error) {
-      res.status(400).json({ error: 'consulta no valida.'});
-    }
-    //Regresar un objeto json con el listado de los personajes.
-    res.status(200).json(results);
+  const connection = mysql.createConnection({
+  connection.connect();
+  
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.json()) // for parsing application/json
+  app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+  
+  app.get('/', (req, res) => {
+    res.send("Bienvenido a la API de Rogelio Noris");
+  @@ -53,6 +55,20 @@ app.get('/personajes/:id', (req, res) => {
+    });
   });
-});
-
-
-app.get('/personajes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  if(isNaN(id)) {
-    res.status(400).json({ error: 'parametros no validos.'});
-    return;
-  }
-  //Consultar los personajes
-  connection.query(`SELECT * FROM personajes WHERE id=?`, [id] ,function (error, results, fields) {
-    if(error) {
-      res.status(400).json({ error: 'consulta no valida.'});
-      return;
-    }
-    if(results.length === 0) {
-      res.status(404).json({ error: 'personaje no existente.'});
-      return;
-    }
-    //Regresar un objeto json con el listado de los personajes.
-    res.status(200).json(results);
+  
+  app.post('/personajes', (req, res) => {
+    console.log("req", req.body);
+    const nombre = req.body.nombre;
+    const rol = req.body.rol;
+    const descripcion = req.body.descripcion;
+    connection.query(`INSERT INTO personajes (nombre, rol, descripcion) VALUES (?,?,?)`, [nombre,rol,descripcion] ,function (error, results, fields) {
+      if(error) {
+        res.status(400).json({ error: 'consulta no valida.'});
+        return;
+      }
+      res.status(200).json({ success: true });
+    });
   });
-});
-
-app.post('/personajes', (req, res) => {
-  console.log("req", req.body);
-  const nombre = req.body.nombre;
-  const rol = req.body.rol;
-  const descripcion = req.body.descripcion;
-  connection.query(`INSERT INTO personajes (nombre, rol, descripcion) VALUES (?,?,?)`, [nombre,rol,descripcion] ,function (error, results, fields) {
-    if(error) {
-      res.status(400).json({ error: 'consulta no valida.'});
-      return;
-    }
-    res.status(200).json({ success: true });
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  }); 
