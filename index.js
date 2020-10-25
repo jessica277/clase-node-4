f462431f3805bff99371ccf3bb7f93552e938c7f
royercross-desktop <rogelio.norisc@gmail.com>
2020-10-25 12:51:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const connection = mysql.createConnection({
 connection.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.send("Bienvenido a la API de Rogelio Noris");
@@ -50,6 +52,20 @@ app.get('/personajes/:id', (req, res) => {
     }
     //Regresar un objeto json con el listado de los personajes.
     res.status(200).json(results);
+  });
+});
+
+app.post('/personajes', (req, res) => {
+  console.log("req", req.body);
+  const nombre = req.body.nombre;
+  const rol = req.body.rol;
+  const descripcion = req.body.descripcion;
+  connection.query(`INSERT INTO personajes (nombre, rol, descripcion) VALUES (?,?,?)`, [nombre,rol,descripcion] ,function (error, results, fields) {
+    if(error) {
+      res.status(400).json({ error: 'consulta no valida.'});
+      return;
+    }
+    res.status(200).json({ success: true });
   });
 });
 
